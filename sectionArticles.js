527b3035f13d7c505c4863fc083b165e2f3a51d2
Oskar Paustian Andersen <oska1204@edu.campusvejle.dk>
2019-12-09 11:26:46 +0100

    Added css grid certain items and a bit of js

--- sectionArticles.js
+++ sectionArticles.js
@@ -19,9 +19,9 @@ class SectionArticles extends HTMLElement {
     }
     set setData(arr) {
         this._data = arr;
+        /*test*/this._data.forEach(e=>{e.discount = Math.round(Math.random() - .25) ? Math.floor(Math.random() * 10) * 10 : 0;e.price = Math.floor(Math.random() * 10) * 10})
         this._createArticles(this._section);
-        // test 
-        this._section.querySelectorAll('img').forEach(e=>e.src = `https://placekitten.com/${500 + 100}/${Math.floor((Math.random() * 300)) + 100}`)
+        /*test*/this._section.querySelectorAll('img').forEach(e=>e.src = `https://placekitten.com/${Math.floor(Math.random() * 5) + 400}/${Math.floor(Math.random() * 3)+ 200}`)
     }
     set setCurrentCategories(arr) {
         this._updateUi(arr);
@@ -45,14 +45,16 @@ class SectionArticles extends HTMLElement {
             }
         });
         const errorNode = this.shadowRoot.querySelector('.error')
+        const section = this.shadowRoot.querySelector('section');
         if (!hasArticle && !errorNode) {
-            const section = this.shadowRoot.querySelector('section');
+            section.style.display = 'none';
             const errorTemplate = document.createElement('template');
             errorTemplate.innerHTML = `<div class="error">
                 <h3 class="error">No articles selected.</h3>
             </div>`;
-            section.appendChild(errorTemplate.content)
+            this.shadowRoot.appendChild(errorTemplate.content)
         } else if (hasArticle && errorNode) {
+            section.style.display = '';
             errorNode.parentNode.removeChild(errorNode);
         }
     }
@@ -61,18 +63,20 @@ class SectionArticles extends HTMLElement {
             const articleTemplate = document.createElement('template');
             articleTemplate.innerHTML = `<article data-categories="${e.categories}">
                 <img class="image" src="${e.images[e.featuredImage]}" alt="${e.images[e.featuredImage]}">
-                <h3 class="secondary-heading">${e.title}</h3>
-                <div class="description">
-                    <p title="${e.description}">${e.description}</p>
-                </div>
-                <ul class="categories">
-                    ${e.categories.map(category => {
-                    return `<li>${category}</li>`
-                    }).join('')}
-                </ul>
-                <div class="price">
-                    <div class="price-tag">Price: <span>${e.price},-</span></div>
-                    ${e.discount ? `<div class="discount">${Math.floor((e.price / (e.discount + e.price) - 1)*-100)}%<span>${e.price + e.discount}</span></div>` : ``}
+                <div class="content">
+                    <h3 class="secondary-heading">${e.title}</h3>
+                    <div class="description">
+                        <p title="${e.description}">${e.description}</p>
+                    </div>
+                    <ul class="categories">
+                        ${e.categories.map(category => {
+                        return `<li>${category}</li>`
+                        }).join('')}
+                    </ul>
+                    <div class="price">
+                        <div class="price-tag">Price: <span>${e.price},-</span></div>
+                        ${e.discount ? `<div class="discount">${Math.floor((e.price / (e.discount + e.price) - 1)*-100)}%<span>${e.price + e.discount}</span></div>` : ``}
+                    </div>
                 </div>
             </article>`
             section.appendChild(articleTemplate.content);
