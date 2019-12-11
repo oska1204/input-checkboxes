0b8cbadbdd15f258808a28096cfb7f9a7e7edaf1
Oskar Paustian Andersen <oska1204@edu.campusvejle.dk>
2019-12-11 12:01:08 +0100

    Added categorie titles

--- manageWebcomponents.js
+++ manageWebcomponents.js
@@ -3,26 +3,42 @@ window.addEventListener('DOMContentLoaded', () => {
     const inputCheckboxes = document.querySelector('input-checkboxes');
     const sectionArticles = document.querySelector('section-articles');
 
-    const sectionArticlesApiSuccess = function(parsedData) {
-        sectionArticles.setData = parsedData.data;
-        sectionArticles.setCurrentCategories = inputCheckboxes.getTags;
-    }
-    const sectionArticlesApiError = function(error) {
-        console.log(error);
-    };
-    
-    const inputCheckboxesApiSuccess = function(parsedData) {
-        inputCheckboxes.setData = parsedData.data.map(obj => {
+    const inputCheckboxesApiSuccess = function(parsedDataInputCheckboxes) {
+        inputCheckboxes.setData = parsedDataInputCheckboxes.data.map(obj => {
             return { 
                 name: obj.title,
                 description: obj.description
             }
         });
+        
+        const sectionArticlesApiSuccess = function(parsedDataSectionArticles) {
+            const theData = parsedDataSectionArticles.data;            
+            theData.forEach(dataElement => {
+                dataElement.categories = dataElement.categories.map(categorie => {
+                    parsedDataInputCheckboxes.data.forEach(e => {
+                        if (categorie === e.id) {
+                            categorie = e.title
+                        }
+                    })
+                    return categorie
+                });
+                console.log(dataElement.categories)
+            });
+            sectionArticles.setData = theData;
+            sectionArticles.setCurrentCategories = inputCheckboxes.getTags;
+        }
+    
+        const sectionArticlesApiError = function(error) {
+            console.log(error);
+        };
+
         api('http://localhost:3000/bikes', sectionArticlesApiSuccess, sectionArticlesApiError);
     }
+
     const inputCheckboxesApiError = function(error) {
         console.log(error);
     };
+
     api('http://localhost:3000/categories', inputCheckboxesApiSuccess, inputCheckboxesApiError);
 
     inputCheckboxes.addEventListener('tags-changed', e => {
